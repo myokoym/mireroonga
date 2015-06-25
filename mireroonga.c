a3e8fa60eaa7445fc1ebf35e5c0e630720193ef9
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-06-25 22:56:10 +0900

    Create grn_ctx

--- mireroonga.c
+++ mireroonga.c
@@ -6,12 +6,17 @@
 int
 main() {
   grn_rc rc;
+  grn_ctx *ctx;
+  int flags = 0;
 
   rc = grn_init();
   if (rc != GRN_SUCCESS) {
     return EXIT_FAILURE;
   }
 
+  ctx = grn_ctx_open(flags);
+
+  grn_ctx_close(ctx);
   grn_fin();
   return EXIT_SUCCESS;
 }
