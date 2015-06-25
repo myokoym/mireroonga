0ef89735cda7a4e13aa14d0b3fca53ffd239d671
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-06-25 23:08:28 +0900

    Open a database

--- mireroonga.c
+++ mireroonga.c
@@ -4,9 +4,10 @@
 #define EXIT_FAILURE 1
 
 int
-main() {
+main(int argc, char **argv) {
   grn_rc rc;
   grn_ctx *ctx;
+  grn_obj *db;
   int flags = 0;
 
   rc = grn_init();
@@ -15,7 +16,10 @@ main() {
   }
 
   ctx = grn_ctx_open(flags);
+  db = grn_db_open(ctx, argv[1]);
+  grn_p(ctx, db);
 
+  grn_obj_unlink(ctx, db);
   grn_ctx_close(ctx);
   grn_fin();
   return EXIT_SUCCESS;
